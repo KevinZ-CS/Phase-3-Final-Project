56b1d40d542e6fed033db428de992cc7bc78dcf4
Kevin Zheng <kevzheng27@gmail.com>
2022-11-02 19:41:45 -0400

    Create due_dates migration

--- todo_list-server/db/migrate/20221102233258_create_tasks.rb
+++ todo_list-server/db/migrate/20221102233258_create_tasks.rb
@@ -1,4 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[6.1]
   def change
+    create_table :tasks do |t|
+      t.string :task
+      t.integer :category_id
+      t.integer :due_date_id
+    end
   end
 end
---
+++ todo_list-server/db/migrate/20221102234105_create_due_dates.rb
@@ -0,0 +1,4 @@
+class CreateDueDates < ActiveRecord::Migration[6.1]
+  def change
+  end
+end

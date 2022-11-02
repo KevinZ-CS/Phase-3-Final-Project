ba171203b2e619ee762f49cd2bf99164752c1d7f
Kevin Zheng <kevzheng27@gmail.com>
2022-11-02 18:01:10 -0400

    Built out layout for NewToDoForm with JSX

--- todo_list-client/src/components/ToDoList.jsx
+++ todo_list-client/src/components/ToDoList.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Card, Button, Col, Form, Dropdown, ListGroup } from 'react-bootstrap'
+import { Container, Row, Card, Button, Form, Dropdown, ListGroup } from 'react-bootstrap'
+import NewToDoForm from "./NewToDoForm";
 
 function ToDoList() {
 
@@ -10,42 +11,7 @@ return (
     <Card>
         <Card.Header className="h2 bg-white">ToDo:</Card.Header>
 
-        <Form className="d-flex px-2" >
-     
-            <Form.Group className="task-input px-2 col-md-4">
-                        <label>Task:</label>
-                    <Form.Control 
-                    type="text"
-                    autoFocus
-                    autoComplete="off"
-                    />
-            </Form.Group>
-
-            <Col className="mt-4 py-1 px-1 d-flex">
-            <input type='date'></input>
-            </Col>
-
-
- 
-            <Dropdown className="mt-4 px-1">
-            <Dropdown.Toggle variant="success">
-                Select Category
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-            </Dropdown.Menu>
-            </Dropdown>
-
-    
-            <Col className="mt-4 px-1">
-            <Button className="add-to-list-btn">Add to List</Button>
-            </Col>
-    
-
-        </Form>
+       <NewToDoForm />
           
     <hr />
 

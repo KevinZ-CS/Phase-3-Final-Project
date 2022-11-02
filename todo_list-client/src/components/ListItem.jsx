74063103d52f125b5c4d2e511d6326f2355a6b4a
Kevin Zheng <kevzheng27@gmail.com>
2022-11-02 18:51:17 -0400

    Built out ListItem component layout with JSX

--- todo_list-client/src/components/ListItem.jsx
+++ todo_list-client/src/components/ListItem.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Row, Card, Button, Form, Dropdown, ListGroup } from 'react-bootstrap'
-import NewToDoForm from "./NewToDoForm";
+import { Button, Form, ListGroup } from 'react-bootstrap'
+
 
 function ListItem() {
 
     return (
+
+    <ListGroup.Item variant="success">
+
+        <span className="d-flex list-item">
+            <Form.Check className="px-2"/>
+            <span className="strike">Chore: Take dog out for a walk</span>
+        </span>
+
+        <span  className="delete-btn">
+        <Button className="" type="button" variant="danger">Delete</Button>
+        </span>  
+
+    </ListGroup.Item>
 
     )
 }
--- todo_list-client/src/components/ToDoList.jsx
+++ todo_list-client/src/components/ToDoList.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row, Card, Button, Form, Dropdown, ListGroup } from 'react-bootstrap'
 import NewToDoForm from "./NewToDoForm";
+import ListItem from "./ListItem";
 
 function ToDoList() {
 
@@ -30,26 +31,7 @@ return (
     </Dropdown>
 
     <ListGroup>
-        
-        <ListGroup.Item variant="success">
-        <span className="d-flex list-item">
-            <Form.Check className="px-2"/>
-            <span className="strike">Chore: Take dog out for a walk</span>
-        </span>
-        <span  className="delete-btn">
-        <Button className="" type="button" variant="danger">Delete</Button>
-        </span>                
-        </ListGroup.Item>
-
-        <ListGroup.Item variant="success">
-        <span className="d-flex list-item">
-            <Form.Check className="px-2"/>
-            <span className="strike">Chore: Take dog out for a walk</span>
-        </span>
-        <span  className="delete-btn">
-        <Button className="" type="button" variant="danger">Delete</Button>
-        </span>                
-        </ListGroup.Item>
+        <ListItem />
         
     </ListGroup>
 

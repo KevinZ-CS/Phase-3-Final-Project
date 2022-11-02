9ff1b9c5fed17e179fd2363fabb298f98f69ca04
Kevin Zheng <kevzheng27@gmail.com>
2022-11-02 19:52:42 -0400

    Add association to task model to due_date model

--- todo_list-server/app/models/category.rb
+++ todo_list-server/app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ActiveRecord::Base
     has_many: tasks
+    has_many :due_dates, through: :tasks
 end
--- todo_list-server/app/models/due_date.rb
+++ todo_list-server/app/models/due_date.rb
@@ -1,3 +1,4 @@
 class Due_date < ActiveRecord::Base
-
+    has_many :tasks
+    has_many :categories, through: :tasks
 end

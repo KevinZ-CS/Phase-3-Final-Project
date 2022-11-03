0fe7f8eac1e9f1f7af08ac4c21bcee10ae3c5db9
Kevin Zheng <kevzheng27@gmail.com>
2022-11-02 22:22:52 -0400

    Add post route to respond to create request in app controller file

--- todo_list-server/app/controllers/application_controller.rb
+++ todo_list-server/app/controllers/application_controller.rb
@@ -7,6 +7,27 @@ class ApplicationController < Sinatra::Base
     tasks.to_json
   end
 
+  post '/tasks' do
+
+    task = Task.create(
+      task: "#{params[:category]}: #{params[:task]} due #{params[:due_date]}"
+      complete: false
+      )
+    category = Category.find_or_create_by(
+      category: params[:category]
+    )
+
+    due_date = Due_date.find_or_create_by(
+      due_date: params[:due_date]
+      )
+
+    category.tasks << task
+    due_date.tasks << task
+
+    task.to_json
+
+  end
+
   delete '/tasks/:id' do
     task = Task.find(params[:id])
     task.destroy
